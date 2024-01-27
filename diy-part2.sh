3b8068de8d9206b142a68e9bd84879acecba23fd
tdzz8 <64826441+tdzz8@users.noreply.github.com>
2024-01-28 01:02:29 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.3.241/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/192.168.3.241/g' package/base-files/files/bin/config_generate
 
 # Modify hostname
 # sed -i 's/OpenWrt/op/g' package/base-files/files/bin/config_generate
